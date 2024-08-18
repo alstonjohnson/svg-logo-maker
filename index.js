e66415b82436ad135ad746b22d63c7efa544ce1d
alstonjohnson <johnson.alston@icloud.com>
2024-08-18 12:53:18 -0500

    began writeToFile function

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 const inquirer = require("inquirer");
 const fs = requre("fs");
-
 const { Triangle, Square, Circle } = require("./lib/shapes");
+
+function writeToFile(fileName, answers) {
+    //possible this needs to be let
+    const svgString = "";
+}
