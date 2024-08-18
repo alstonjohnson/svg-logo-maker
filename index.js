43f6adb4b3808e5b7848cc9dc29bac33c6f12742
alstonjohnson <johnson.alston@icloud.com>
2024-08-18 13:05:15 -0500

    continued working on writeToFile

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const fs = requre("fs");
 const { Triangle, Square, Circle } = require("./lib/shapes");
 
 function writeToFile(fileName, answers) {
-    //possible this needs to be let
+    //possible this needs to be let. this is the file starting as an empty string
     const svgString = "";
+    //Below is likely to be a problem spot. Might not need that slash after "2"
+    svgString = '<svg version="1.1" width="300" height="200" xmlns="https://www.w3.org/TR/SVG2/">';
 }
